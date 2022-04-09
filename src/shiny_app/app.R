b177c355b825c3ee1e30c3952d739fa78542c951
Yunfan Ling <superlingyunfan@gmail.com>
2022-04-09 17:07:59 -0400

    update the shiny app

--- src/shiny_app/app.R
+++ src/shiny_app/app.R
@@ -2,9 +2,9 @@ library(shiny)
 library(shinythemes)
 library(ggplot2)
 library(tidyverse)
+library(readr)
+library(ggthemes)
 library(plotly)
-library (readr)
-
 
 newdf<-read.csv("https://raw.githubusercontent.com/supertrashpanda/BeautifulBars/main/data/new_long_inc_data.csv",check.names=FALSE)
 
@@ -28,9 +28,9 @@ ui <- fluidPage(
       # Input: Slider for the number of bins ----
       sliderInput("yearend",
                   label = "",
-                  min = 1990,
-                  max = 2020,
-                  value =c(1999.49,2020),
+                  min = 1989,
+                  max = 2021,
+                  value =c(1999.49,2021),
                   sep=""),
       selectInput(inputId = "education1", label = "Choose one education level:", 
                   choices = list("Less than 9th grade" = "Less than 9th grade", "Some high school, no completion" = "Some high school, no completion", "High school completion" = "High school completion",
@@ -43,13 +43,13 @@ ui <- fluidPage(
                                  "Professional degree"="Professional degree","Doctor's degree"="Doctor's degree"), 
                   selected = "Bachelor's degree"),
       checkboxInput("male","Male", TRUE), 
-      checkboxInput("female","Female", TRUE)
-      
+      checkboxInput("female","Female", TRUE),
+      hr(),
+      helpText("Data from the National Center for Education Statistics")
     ),
     
     # Main panel for displaying outputs ----
     mainPanel(
-    
       plotOutput(outputId = "plot"),
       htmlOutput("txtout")
       
@@ -74,7 +74,7 @@ server <- function(input, output,session) {
     if((!input$male)&(!input$female)) {newdf<-newdf[newdf$sex=='',]}
 
     a<-newdf[which((newdf$education%in%c(input$education1,input$education2))&(newdf$money_measure=="Constant 2019 Dollars")),]%>%select(-idtf)
-    a$year<-ifelse(a$sex=="Male",a$year-0.15,a$year+0.15)
+    a$year<-ifelse(a$sex=="Male",a$year-0.1,a$year+0.1)
     a<-a[which((a$year>=input$yearend[1])&(a$year<=input$yearend[2])),]
     male<-a%>%spread(education,avg_income)%>%filter(sex=="Male")
     names(male)<-make.names(names(male),unique = TRUE)
@@ -99,7 +99,7 @@ server <- function(input, output,session) {
     HTML(paste(str1, str2,str3, sep = '<br/>'))
   })
   
-    
+  
     output$plot <- renderPlot({
         df_gap()$a%>%ggplot()+
         geom_smooth(aes(year,avg_income,fill=education),size=0,span=1,alpha=0.1)+
@@ -111,13 +111,17 @@ server <- function(input, output,session) {
                      aes_string(x="year", xend="year",y=colnames(df_gap()$female)[5],yend=colnames(df_gap()$female)[6]),colour = "hotpink2",size=0.5)+
         geom_point(aes(year,avg_income,color=sex,shape=education),size=1.5,alpha=1,fill="white")+
         scale_shape_manual(values=c(21, 16))+
-        ylim(0,200000)+scale_x_continuous(breaks=seq(1990, 2020, 1))+
+        scale_x_continuous(breaks=seq(1989, 2021, 2),minor_breaks=seq(1989, 2021, 2))+
+
+        scale_y_continuous(breaks=seq(0, 200000, 50000),minor_breaks=seq(0, 200000, 50000))+
+        ylim(0,200000)+
+        geom_hline(yintercept=0,alpha=0.5,size=1)+
         labs(x="Year",y="Median Annual Income (in fixded 2019 dollars)",shape="Education Levels\nto compare",
              fill="Education Levels\nto compare",color="Sex\n(with different colors)")+
         theme_minimal()+
-        theme(legend.position = c(0.6, 0.98),
-              legend.justification=c(0.5, 1),legend.box = "horizontal")+
-        theme(axis.text.x = element_text(angle=45,size=6))
+        theme(legend.position = c(0.6, 0.95),legend.justification=c(0.5, 1),
+              legend.box = "horizontal",axis.text.x = element_text(size=8,angle=45),
+              panel.grid.minor.y = element_blank(),panel.grid.major.x = element_blank())
       })
 
 }
